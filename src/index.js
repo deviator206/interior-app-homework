84fa5b32d8aec346305b9e829555ff85029e6a54
Sandeep Bamane <sb@Sandeeps-Mac-mini.local>
2020-05-25 11:13:01 +0530

    fixed on touch events

--- src/index.js
+++ src/index.js
@@ -35,15 +35,16 @@ window.onload = () => {
 
   const onMouseMove = function (e) {
     if (initiateDragging) {
-      mouse.x = e.pageX - this.offsetLeft;
-      mouse.y = e.pageY - this.offsetTop;
+      mouse.x = ((e.touches) ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
+      mouse.y = ((e.touches) ? e.touches[0].pageY : e.pageY) - this.offsetTop;
     } else if (drawHorizontal) {
       mouse.y = mouseY;
-      mouse.x = e.pageX - this.offsetLeft;
+      mouse.x = ((e.touches) ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
     } else if (drawVertical) {
-      mouse.y = e.pageY - this.offsetTop;
+      mouse.y = ((e.touches) ? e.touches[0].pageY : e.pageY) - this.offsetTop;
       mouse.x = mouseX;
     }
+    // onPaint();
   }
   const onMouseOut = function (e) {
     initiateMouseDown = false;
@@ -54,6 +55,7 @@ window.onload = () => {
   const onMouseUp = function () {
     initiateMouseDown = false;
     canvas.removeEventListener('mousemove', onPaint, false);
+    canvas.removeEventListener("touchmove", onPaint, false);
   }
 
   const onMouseDown = function (e) {
@@ -62,15 +64,21 @@ window.onload = () => {
       mouseY = -1;
       initiateMouseDown = true;
       if (drawHorizontal) {
-        mouseY = e.pageY - this.offsetTop;
+        mouseY = ((e.touches) ? e.touches[0].pageY : e.pageY) - this.offsetTop;
+        mouse.x = ((e.touches) ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
       } else if (drawVertical) {
-        mouseX = e.pageX - this.offsetLeft;
+        mouseX = ((e.touches) ? e.touches[0].pageX : e.pageX) - this.offsetLeft;
+        mouse.y = ((e.touches) ? e.touches[0].pageY : e.pageY) - this.offsetTop;
       }
 
     }
     ctx.beginPath();
     ctx.moveTo((mouseX !== -1) ? mouseX : mouse.x, (mouseY !== -1) ? mouseY : mouse.y);
-    canvas.addEventListener('mousemove', onPaint, false);
+    if (e.touches && e.touches[0]) {
+      canvas.addEventListener("touchmove", onPaint, false);
+    } else {
+      canvas.addEventListener('mousemove', onPaint, false);
+    }
   }
 
   var onPaint = function () {
@@ -107,13 +115,13 @@ window.onload = () => {
 
 
   canvas.addEventListener('mousemove', onMouseMove, false);
-  canvas.addEventListener('mouseout', onMouseOut, false);
+  canvas.addEventListener('mouseout', onMouseUp, false);
   canvas.addEventListener('mousedown', onMouseDown, false);
   canvas.addEventListener('mouseup', onMouseUp, false);
 
   canvas.addEventListener("touchstart", onMouseDown, false);
   canvas.addEventListener("touchend", onMouseUp, false);
-  canvas.addEventListener("touchcancel", onMouseOut, false);
+  canvas.addEventListener("touchcancel", onMouseUp, false);
   canvas.addEventListener("touchmove", onMouseMove, false);
 
 }
